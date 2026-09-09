b4172042f9af5ce3aa846f12734aad33ea28426a
HaikalMuzaki <haikalmuzaki28@gmail.com>
2026-09-09 14:33:09 +0700

    Update price and packages

--- registration/views.py
+++ registration/views.py
@@ -456,10 +456,10 @@ def _create_checkout_transaction(request, package_type, *, cohort_year_override=
         total_amount = Decimal('0')
         for index in range(quantity):
             if package_type == 'ALUMNI_PACK':
-                price = Decimal('225000')
+                price = Decimal('200000')
                 tshirt_size = tshirt_sizes[index]
             elif package_type == 'STUDENT_PACK':
-                price = Decimal('125000')
+                price = Decimal('150000')
                 tshirt_size = tshirt_sizes[index]
             else:
                 price = Decimal('50000')
